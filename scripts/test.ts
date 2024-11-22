abfb134a8b2fbdfc3f9367c467f4031cf4a85b53
fernandodeluret <fernandodeluret@gmail.com>
2024-11-22 17:44:56 -0300

    `fix`

--- scripts/test.ts
+++ scripts/test.ts
@@ -1,45 +1,45 @@
-import { getAccount, getAssociatedTokenAddressSync } from "@solana/spl-token";
-import { initialSetup } from "../src/app/utils/initialSetup";
-import { PublicKey, VersionedTransaction } from "@solana/web3.js";
-import { toUint8Array } from "js-base64";
+// import { getAccount, getAssociatedTokenAddressSync } from "@solana/spl-token";
+// import { initialSetup } from "../src/app/utils/initialSetup";
+// import { PublicKey, VersionedTransaction } from "@solana/web3.js";
+// import { toUint8Array } from "js-base64";
 
-// const config = require("../src/app/utils/initialSetup");
+// // const config = require("../src/app/utils/initialSetup");
 
-const test = async () => {
-  const { wallet, connection, usdcDevMint, mintDecimals } =
-    await initialSetup();
-  console.log(wallet.publicKey.toBase58());
+// const test = async () => {
+//   const { wallet, connection, usdcDevMint, mintDecimals } =
+//     await initialSetup();
+//   console.log(wallet.publicKey.toBase58());
 
-  const amount = 1500;
-  const url = `http://localhost:3001/api/actions/place-bid?auction=Ant8qoUbnsYd54cywvUhWND4RtqSspeAeu3537WWSjG3&amount=${amount}`;
-  const res = await fetch(url, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      account: "8MbJpTkFxpnQcxs1No1WLhvAfAz8pZViw1yJRbcWDU2M",
-    }),
-  });
+//   const amount = 1500;
+//   const url = `http://localhost:3001/api/actions/place-bid?auction=Ant8qoUbnsYd54cywvUhWND4RtqSspeAeu3537WWSjG3&amount=${amount}`;
+//   const res = await fetch(url, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({
+//       account: "8MbJpTkFxpnQcxs1No1WLhvAfAz8pZViw1yJRbcWDU2M",
+//     }),
+//   });
 
-  const ata = getAssociatedTokenAddressSync(usdcDevMint, wallet.publicKey);
-  const ataData = await getAccount(connection, ata);
-  console.log("ataData.amount: ", ataData.amount);
-  console.log("mintDecimals: ", mintDecimals);
+//   const ata = getAssociatedTokenAddressSync(usdcDevMint, wallet.publicKey);
+//   const ataData = await getAccount(connection, ata);
+//   console.log("ataData.amount: ", ataData.amount);
+//   console.log("mintDecimals: ", mintDecimals);
 
-  const payload = await res.json();
-  console.log(payload);
+//   const payload = await res.json();
+//   console.log(payload);
 
-  const serializedTx = payload["transaction"];
-  const tx = VersionedTransaction.deserialize(toUint8Array(serializedTx));
-  tx.sign([wallet]);
+//   const serializedTx = payload["transaction"];
+//   const tx = VersionedTransaction.deserialize(toUint8Array(serializedTx));
+//   tx.sign([wallet]);
 
-  const sig = await connection.sendTransaction(tx);
-  await connection.confirmTransaction(sig, "confirmed");
-  console.log("sig: ", sig);
-  console.log("testPlaceBid done!");
-};
+//   const sig = await connection.sendTransaction(tx);
+//   await connection.confirmTransaction(sig, "confirmed");
+//   console.log("sig: ", sig);
+//   console.log("testPlaceBid done!");
+// };
 
-test()
-  .then(() => console.log("ok"))
-  .catch((e) => console.log(e));
+// test()
+//   .then(() => console.log("ok"))
+//   .catch((e) => console.log(e));
